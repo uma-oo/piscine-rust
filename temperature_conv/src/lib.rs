2b3f1e5acaf0216a9eb7e735069a8efe66e16db4
uma-oo <oumayma.elfahsi99@gmail.com>
2025-09-04 13:44:27 +0100

    add the temperature

---
+++ temperature_conv/src/lib.rs
@@ -0,0 +1,8 @@
+pub fn fahrenheit_to_celsius(f: f64) -> f64 {
+    return ((f-32 as f64)*5 as f64/9 as f64);
+
+}
+
+pub fn celsius_to_fahrenheit(c: f64) -> f64 {
+    return c*(9 as f64 /5 as f64)+ 32 as f64;
+}
