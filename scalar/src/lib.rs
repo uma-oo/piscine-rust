09d1a2057dbb9dcceabbd72b7e1037c9a891905c
uma-oo <oumayma.elfahsi99@gmail.com>
2025-09-04 13:19:22 +0100

    add scalar

---
+++ scalar/src/lib.rs
@@ -0,0 +1,33 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+pub fn sum(a: u8 , b:u8)-> u8 {
+    return a+b;
+}
+
+
+pub fn diff(a: i16 , b:i16)-> i16 {
+    return a-b;
+}
+
+pub fn pro(a: i8 , b:i8)-> i8 {
+    return a*b;
+}
+
+
+//  don't we need to check if the b sghr mn 0 ??
+pub fn quo(a: f32 , b:f32)-> f32 {
+    return a/b;
+}
+
+pub fn rem(a: f32 , b:f32)-> f32 {
+    return a%b;
+}
+
+
+
+
+
+
+
