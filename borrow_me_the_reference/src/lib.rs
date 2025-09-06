d056fb417ebabb5a9da41684fa1e02b85820ba30
uma-oo <oumayma.elfahsi99@gmail.com>
2025-09-06 11:05:06 +0100

    add code for the borrow_me_reference

--- borrow_me_the_reference/src/lib.rs
+++ borrow_me_the_reference/src/lib.rs
@@ -2,21 +2,57 @@
 
 
 pub fn delete_and_backspace(s: &mut String) {
-    let mut new_string = String::new();
-     for (i , c) in s.clone().char_indices() {
-        if c =='-' {
-            new_string.push_str(&s[..i-1]);
-            new_string.push_str(&s[i+1..]);
-            *s = new_string.clone();
-            new_string.clear();  
-            delete_and_backspace(s);
+  let mut result : Vec<char> = s.chars().collect();
+  let mut i = 0;
+  while i<result.len() {
+    if result[i]=='-' {
+        if i==0 {
+            result.remove(i);
+        } else {
+            result.remove(i);
+            result.remove(i-1);
+
         }
+        
+        i=0;
+    } else if result[i]=='+' &&  i+1 < result.len() &&  result[i+1]!='+'{
+            result.remove(i+1);
+            result.remove(i);
+            i=0;
+     } else if result[i]=='+' && i==result.len() -1{
+            result.remove(i);
+            i=0;
+         }   else {
+        i+=1;
     }
-    //println!("{}", new_string);
-    //  println!("{}", new_string);
+
+   
+  }
+
+  *s = result.into_iter().collect();
    
 }
 
 pub fn do_operations(v: &mut [String]) {
-    println!("{:?}", v)
-}
+    for element in v {
+    let chars_element : Vec<char>  = element.chars().collect();
+    let mut i = 0;
+    for &c in chars_element.iter() {
+        if c == '-'  ||  c =='+' {
+          let parsed_left : String=  chars_element[0..i].iter().collect();
+          let  parsed_left : i32= parsed_left.parse().unwrap();
+          let parsed_right : String=  chars_element[i+1..].iter().collect();
+          let  parsed_right : i32= parsed_right.parse().unwrap();
+         if c == '+' {
+            *element = (parsed_left+parsed_right).to_string();
+         } else {
+            *element = (parsed_left-parsed_right).to_string();
+         }
+
+        }
+        i+=1;
+    }
+    }
+
+    
+    }
--- borrow_me_the_reference/src/main.rs
+++ borrow_me_the_reference/src/main.rs
@@ -1,9 +1,7 @@
-
-
 use borrow_me_the_reference::*;
 
 fn main() {
-    let mut a = "bpp--o+er+++sskroi-++lcw".to_owned();
+    let mut a = "--++++".to_owned();
     let mut b = [
         "2+2".to_owned(),
         "3+2".to_owned(),
@@ -15,4 +13,42 @@ fn main() {
     do_operations(&mut b);
 
     println!("{:?}", (a, b));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_delete_and_backspace() {
+        let mut a_1 = String::from("bpp--o+er+++sskroi-++lcw");
+        let mut a_2 = String::from("hs-+deasdasd------l+++dsdp");
+        let mut a_3 = String::from("pad-rtic+eulqw--+rar");
+        let mut a_4 = String::from("--++++");
+
+        delete_and_backspace(&mut a_1);
+        delete_and_backspace(&mut a_2);
+        delete_and_backspace(&mut a_3);
+        delete_and_backspace(&mut a_4);
+
+        assert_eq!(a_1, "borrow");
+        assert_eq!(a_2, "help");
+        assert_eq!(a_3, "particular");
+        assert_eq!(a_4, "");
+    }
+
+    #[test]
+    fn test_do_operations() {
+        let mut b_1 = [
+            "2+2".to_owned(),
+            "3+2".to_owned(),
+            "10-3".to_owned(),
+            "0+0".to_owned(),
+            "0-0".to_owned(),
+            "10-100".to_owned(),
+        ];
+        do_operations(&mut b_1);
+
+        assert_eq!(b_1, ["4", "5", "7", "0", "0", "-90"]);
+    }
 }
