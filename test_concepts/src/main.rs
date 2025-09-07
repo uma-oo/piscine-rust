009784eeca52bd21e106fd21043dfbae635cde41
uma-oo <oumayma.elfahsi99@gmail.com>
2025-09-07 17:30:29 +0100

    add some code for the learning sake

--- test_concepts/src/main.rs
+++ test_concepts/src/main.rs
@@ -1,17 +1,36 @@
-
-
-
-
+use std::io;
+use rand::prelude::*;
 
 
 
 fn main(){
-    let outer_ref : &u32 ;
-    {
-        let inner_ref: u32 = 5;
-        outer_ref = &inner_ref;
-        println!("{}", outer_ref)
-    }
+    let guessed = rand::thread_rng().gen_range(0..=10);
+    loop {
+        let mut number = String::new();
+        match io::stdin().read_line(& mut number) {
+            Ok(_)=> {
+            let  number : i32 = match number.trim().parse() {
+            Ok(num)=> num,
+            Err(err)=> {
+                println!("there was an error while trying to convert the number!! {}", err);
+                continue
+            }
+        };
 
-    println!("{}",outer_ref)
+        if number == guessed {
+            println!("Hey u did well");
+            break;
+        } else {
+            println!("Hey u did not do well");
+
+        }
+            },
+            Err(e) => {
+                println!("there was an error while reading the file! {:?}", e);
+                continue
+            }
+        };
+       
+    }
+    
 }
