c884fee9a6fb26498aae94d45bbf9a5031e02ae3
uma-oo <oumayma.elfahsi99@gmail.com>
2025-09-11 18:52:47 +0100

    add some code

--- question_mark/src/lib.rs
+++ question_mark/src/lib.rs
@@ -1,18 +1,22 @@
 pub struct One {
     // expected public fields
+    pub first_layer :Option<Two>,
 }
 pub struct Two {
     // expected public fields
+    pub second_layer :Option<Three>
 }
 pub struct Three {
     // expected public fields
+    pub third_layer : Option<Four>
 }
 pub struct Four {
     // expected public fields
+    pub fourth_layer : Option<u16>
 }
 
 impl One {
     pub fn get_fourth_layer(self) -> Option<u16> {
-        todo!()
+       self.first_layer?.second_layer?.third_layer?.fourth_layer
     }
 }
