55fdd6684f4631d3951ff71a42873dc7d6b45ecc
uma-oo <oumayma.elfahsi99@gmail.com>
2025-09-13 19:09:28 +0100

    add some code

---
+++ scores/src/lib.rs
@@ -0,0 +1,19 @@
+
+
+pub fn score(word: &str) -> u64 {
+    let mut result = 0;
+    for c in word.to_uppercase().chars() {
+        result += match c {
+            'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' => 1,
+            'D' | 'G' => 2,
+            'B' | 'C' | 'M' | 'P' => 3,
+            'F' | 'H' | 'V' | 'W' | 'Y' => 4,
+            'K' => 5,
+            'J' | 'X' => 8,
+            'Q' | 'Z' => 8,
+            _ => 0,
+        };
+    }
+
+    result
+}
