acf7c590c244da5eaeb9a252bc6e84f47bb26473
uma-oo <oumayma.elfahsi99@gmail.com>
2025-09-14 10:23:10 +0100

    add edits

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -3,10 +3,11 @@ use std::collections::HashSet;
 pub fn is_pangram(s: &str) -> bool {
     let mut letters = HashSet::new();
 
-    for c in s.chars() {
-        if c.is_alphabetic() {
+    for c in s.to_lowercase().chars() {
+        if c.is_ascii_alphabetic() {
             letters.insert(c);
         }
     }
+    println!("{:?}", letters);
     letters.len()==26
 }
