569ad1d49e226c20bd406786df12c60c347f3ad4
uma-oo <oumayma.elfahsi99@gmail.com>
2025-09-14 09:51:44 +0100

    add some code

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -1,10 +1,8 @@
 pub fn num_to_ordinal(x: u32) -> String {
-
-    let  ordinal = match true {
-        _ if x / 10 == 1 => "th".to_string(),
-        _ if x % 10 == 1 => "st".to_string(),
-        _ if x % 10 == 1 => "nd".to_string(),
-        _ if x % 10 == 3 => "rd".to_string(),
+    let ordinal = match true {
+        _ if x % 100 != 11 && x % 10 == 1 => "st".to_string(),
+        _ if x % 100 != 12 && x % 10 == 2 => "nd".to_string(),
+        _ if x % 100 != 13 && x % 10 == 3 => "rd".to_string(),
         _ => "th".to_string(),
     };
 
--- ordinal/src/main.rs
+++ ordinal/src/main.rs
@@ -1,8 +1,19 @@
 use ordinal::*;
 
-fn main() {
-    println!("{}", num_to_ordinal(1));
-    println!("{}", num_to_ordinal(22));
-    println!("{}", num_to_ordinal(43));
-    println!("{}", num_to_ordinal(47));
+#[test]
+fn test_num_to_ordinal() {
+    assert_eq!(num_to_ordinal(0), "0th");
+    assert_eq!(num_to_ordinal(1), "1st");
+    assert_eq!(num_to_ordinal(11), "11th");
+    assert_eq!(num_to_ordinal(12), "12th");
+    assert_eq!(num_to_ordinal(13), "13th");
+    assert_eq!(num_to_ordinal(14), "14th");
+    assert_eq!(num_to_ordinal(22), "22nd");
+    assert_eq!(num_to_ordinal(43), "43rd");
+    assert_eq!(num_to_ordinal(67), "67th");
+    assert_eq!(num_to_ordinal(113), "113th");
+    assert_eq!(num_to_ordinal(114), "114th");
+    assert_eq!(num_to_ordinal(1901), "1901st");
+    assert_eq!(num_to_ordinal(1113), "1113th");
+    assert_eq!(num_to_ordinal(11111), "11111th");
 }
