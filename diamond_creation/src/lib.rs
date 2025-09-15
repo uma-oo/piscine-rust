d9b14f8a57e1965bfdd6746093d0f0087df639b8
uma-oo <oumayma.elfahsi99@gmail.com>
2025-09-15 18:39:38 +0100

    add some edits

--- diamond_creation/src/lib.rs
+++ diamond_creation/src/lib.rs
@@ -7,21 +7,25 @@ pub fn get_diamond(c: char) -> Vec<String> {
         let mut line = String::new();
         line.push_str(&" ".repeat((c_index - i) as usize));
         line.push((('A' as u8) + (i as u8)) as char);
+
         if i > 0 {
+            println!("char :{:?} len {} i: {}", (('A' as u8) + (i as u8)) as char, len, i);
             line.push_str(&" ".repeat((2 * i - 1) as usize));
             line.push((('A' as u8) + (i as u8)) as char);
-        } else {
+            line.push_str(&" ".repeat((c_index - i) as usize));
+        } else if i == 0 {
             line.push_str(&" ".repeat((c_index - i) as usize));
         }
+
         result.push(line);
     }
 
     let cloned = result.clone();
-    let  reversed: Vec<_> = cloned[0..(len - 1) as usize].into_iter().rev().collect();
+    println!("cloned: {:?}", cloned);
+    let reversed: Vec<_> = cloned[0..(len - 1) as usize].into_iter().rev().collect();
     for item in reversed {
         result.push(item.to_string());
     }
-    
+
     result
-   
 }
