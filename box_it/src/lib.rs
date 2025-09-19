c039f60868d62ff2c47a29a09e57852875580a1c
uma-oo <oumayma.elfahsi99@gmail.com>
2025-09-19 11:15:40 +0100

    add some code

--- box_it/src/lib.rs
+++ box_it/src/lib.rs
@@ -1,19 +1,17 @@
 pub fn parse_into_boxed(s: String) -> Vec<Box<u32>> {
-    let mut result: Vec<Box<f32>> = Vec::new();
+    let mut result: Vec<Box<u32>> = Vec::new();
     let splitted: Vec<_> = s.split(" ").collect();
     for val in splitted {
         if val.ends_with("k") {
-            result.push(Box::new(val[0..val.len() - 1].parse::<f32>().unwrap()));
+            let value: u32 = (val[0..val.len() - 1].parse::<f32>().unwrap() * 1000.0)  as u32;
+            result.push(Box::new(value));
+        } else {
+            let value: u32 = val.parse().unwrap();
+            result.push(Box::new(value));
         }
     }
 
-    let mut result_int: Vec<Box<u32>> = Vec::new();
-
-    for box_float in result {
-        result_int.push(Box::new((*box_float * 1000.0) as u32));
-    }
-
-    result_int
+    result
 }
 
 pub fn into_unboxed(a: Vec<Box<u32>>) -> Vec<u32> {
