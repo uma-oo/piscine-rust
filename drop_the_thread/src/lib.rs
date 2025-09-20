f400ed1f342fb048886aadb2c3a4c1ab9db5fcb7
uma-oo <oumayma.elfahsi99@gmail.com>
2025-09-20 12:43:38 +0100

    add some code

--- drop_the_thread/src/lib.rs
+++ drop_the_thread/src/lib.rs
@@ -8,39 +8,63 @@ pub struct ThreadPool {
 
 impl ThreadPool {
     pub fn new() -> Self {
-        todo!()
+        Self {
+            drops: Cell::new(0),
+            states: RefCell::new(Vec::new()),
+        }
     }
 
     pub fn new_thread(&self, c: String) -> (usize, Thread) {
-        todo!()
+        let id = self.states.borrow().len();
+        self.states.borrow_mut().push(false);
+        (id, Thread::new(id, c, &self))
     }
 
     pub fn thread_len(&self) -> usize {
-        todo!()
+        self.states.borrow().len()
     }
 
     pub fn is_dropped(&self, id: usize) -> bool {
-        todo!()
+        self.states.borrow()[id] == true
     }
 
     pub fn drop_thread(&self, id: usize) {
-        todo!()
+        match true {
+            _ if self.states.borrow_mut()[id] == false => {
+                self.states.borrow_mut()[id] = true;
+                self.drops.replace(self.drops.get() + 1);
+            }
+            _ => {
+                panic!("{} is already dropped", id);
+            }
+        }
     }
 }
 
 #[derive(Debug)]
-pub struct Thread {
+pub struct Thread<'a> {
     // expected public fields
+    pub pid: i32,
+    pub cmd: String,
+    pub parent: &'a ThreadPool,
 }
 
 impl<'a> Thread<'a> {
     pub fn new(p: usize, c: String, t: &'a ThreadPool) -> Self {
-        todo!()
+        Self {
+            pid: p as i32,
+            cmd: c,
+            parent: t,
+        }
     }
 
     pub fn skill(self) {
-        todo!()
+        drop(self);
     }
 }
 
-impl Drop for Thread<'_> {}
+impl Drop for Thread<'_> {
+    fn drop(&mut self) {
+        self.parent.drop_thread(self.pid as usize);
+    }
+}
--- drop_the_thread/src/main.rs
+++ drop_the_thread/src/main.rs
@@ -1,23 +1,64 @@
 use std::rc::Rc;
+
 use drop_the_thread::*;
 
-fn main() {
-    let pool = ThreadPool::new();
-    let (id, thread) = pool.new_thread(String::from("command"));
-    let (id1, thread1) = pool.new_thread(String::from("command1"));
+#[test]
+fn tests_drops_are_correct() {
+    let worker = ThreadPool::new();
+    let (pid, thread) = worker.new_thread(String::from("gnome-shell"));
+    let (pid0, thread0) = worker.new_thread(String::from("i3"));
+    let (pid1, thread1) = worker.new_thread(String::from("shell"));
+    let (pid2, thread2) = worker.new_thread(String::from("spotify"));
 
     thread.skill();
+    assert_eq!(worker.drops.get(), 1);
+    thread0.skill();
 
-    println!("{:?}", (pool.is_dropped(id), id, &pool.drops));
+    assert!(worker.is_dropped(pid), "{} should have been dropped", pid);
+    assert!(worker.is_dropped(pid0), "{} should have been dropped", pid0);
+    assert!(
+        !worker.is_dropped(pid1),
+        "{} should not have been dropped",
+        pid1
+    );
+    assert!(
+        !worker.is_dropped(pid2),
+        "{} should not have been dropped",
+        pid2
+    );
+
+    assert_eq!(worker.drops.get(), 2);
 
     thread1.skill();
-    println!("{:?}", (pool.is_dropped(id1), id1, &pool.drops));
+    thread2.skill();
 
-    let (id2, thread2) = pool.new_thread(String::from("command2"));
-    let thread2 = Rc::new(thread2);
-    let thread2_clone = thread2.clone();
+    assert_eq!(worker.drops.get(), 4);
+}
 
-    drop(thread2_clone);
+#[test]
+fn test_drops_only_happen_without_further_references() {
+    let worker = ThreadPool::new();
+    let (_, thread) = worker.new_thread(String::from("Xorg"));
+    let thread = Rc::new(thread);
 
-    println!("{:?}", (pool.is_dropped(id2), id2, &pool.drops, Rc::strong_count(&thread2)));
+    {
+        let _thread_clone = Rc::clone(&thread);
+    }
+
+    assert_eq!(worker.drops.get(), 0);
+}
+
+#[test]
+#[should_panic(expected = "0 is already dropped")]
+fn test_dropping_dropped_thread_panics() {
+    let worker = ThreadPool::new();
+    let (_pid, thread) = worker.new_thread(String::from("gsd-rfkill"));
+    thread.skill();
+
+    let thread_clone = Thread {
+        pid: 0,
+        cmd: "gsd-rfkill".to_owned(),
+        parent: &worker,
+    };
+    thread_clone.skill();
 }
