675efeae1bc06543a002ffac93e2c80e39b426dd
uma-oo <oumayma.elfahsi99@gmail.com>
2025-09-21 15:24:27 +0100

    add some code

---
+++ lalgebra_scalar/src/lib.rs
@@ -0,0 +1,65 @@
+pub trait Scalar: Sized {
+    type Item;
+    fn zero() -> Self::Item;
+    fn one() -> Self::Item;
+}
+
+impl Scalar for i32 {
+    type Item = i32;
+    fn zero() -> Self::Item {
+        0
+    }
+    fn one() -> Self::Item {
+        1
+    }
+}
+
+impl Scalar for i64 {
+    type Item = i64;
+    fn zero() -> Self::Item {
+        0
+    }
+    fn one() -> Self::Item {
+        1
+    }
+}
+
+impl Scalar for u32 {
+    type Item = u32;
+    fn zero() -> Self::Item {
+        0
+    }
+    fn one() -> Self::Item {
+        1
+    }
+}
+
+impl Scalar for u64 {
+    type Item = u64;
+    fn zero() -> Self::Item {
+        0
+    }
+    fn one() -> Self::Item {
+        1
+    }
+}
+
+impl Scalar for f64 {
+    type Item = f64;
+    fn zero() -> Self::Item {
+        0.0
+    }
+    fn one() -> Self::Item {
+        1.0
+    }
+}
+
+impl Scalar for f32 {
+    type Item = f64;
+    fn zero() -> f64 {
+        0.0
+    }
+    fn one() -> f64 {
+        1.0
+    }
+}
