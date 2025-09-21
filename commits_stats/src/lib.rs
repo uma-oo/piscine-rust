4c043c1647443726e757210e28fb612796b64f9e
uma-oo <oumayma.elfahsi99@gmail.com>
2025-09-21 17:21:39 +0100

    ouma

---
+++ commits_stats/src/lib.rs
@@ -0,0 +1,14 @@
+use json;
+use std::collections::HashMap;
+
+pub fn commits_per_week(data: &json::JsonValue) -> HashMap<String, u32> {
+    let commits_week: HashMap<String, u32> = HashMap::new();
+
+    commits_week
+}
+
+pub fn commits_per_author(data: &json::JsonValue) -> HashMap<String, u32> {
+    let commits_users: HashMap<String, u32> = HashMap::new();
+
+    commits_users
+}
