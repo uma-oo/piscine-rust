54793575723e8fc9f830046a2366e466a9296824
uma-oo <oumayma.elfahsi99@gmail.com>
2025-09-25 18:39:46 +0100

    add some code

---
+++ matrix_ops/src/lib.rs
@@ -0,0 +1,49 @@
+// use matrix::Matrix;
+use lalgebra_scalar::*;
+use std::ops::Add;
+use std::ops::Sub;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Matrix<T>(pub Vec<Vec<T>>);
+
+impl<T:  Add<Output = T> + std::fmt::Debug + Copy> Add for Matrix<T> {
+    type Output = Option<Self>;
+    fn add(self, other: Self) -> Self::Output {
+        // hna check rows + columns
+        if self.0.len() != other.0.len() || self.0[0].len() != other.0[0].len() {
+            return None;
+        }
+        let mut rows: Vec<Vec<T>> = Vec::new();
+        for i in 0..self.0.len() {
+            let mut columns: Vec<T> = Vec::new();
+            for j in 0..self.0[0].len() {
+                let result = self.0[i][j] + other.0[i][j];
+                columns.push(result);
+            }
+            rows.push(columns);
+        }
+
+        Some(Matrix(rows))
+    }
+}
+
+impl<T: Sub<Output = T> + std::fmt::Debug + Copy> Sub for Matrix<T> {
+    type Output = Option<Self>;
+    fn sub(self, other: Self) -> Self::Output {
+        if self.0.len() != other.0.len() || self.0[0].len() != other.0[0].len() {
+            return None;
+        }
+        let mut rows = Vec::new();
+        for i in 0..self.0.len() {
+            let mut columns = Vec::new();
+            for j in 0..self.0[0].len() {
+                let result: T = self.0[i][j] - other.0[i][j];
+                println!("{:?}", result);
+                columns.push(result);
+            }
+            rows.push(columns);
+        }
+
+        Some(Matrix(rows))
+    }
+}
---
+++ matrix_ops/src/main.rs
@@ -0,0 +1,23 @@
+
+
+// Importing Matrix by defining it as a dependency in Cargo.toml
+use matrix_ops::*;
+
+
+fn main() {
+	let matrix = Matrix(vec![vec![8, 1], vec![9, 1]]);
+	let matrix_2 = Matrix(vec![vec![1, 1], vec![1, 1]]);
+	println!("{:?}", matrix+matrix_2);
+
+	// let matrix = Matrix(vec![vec![1, 3], vec![2, 5]]);
+	// let matrix_2 = Matrix(vec![vec![3, 1], vec![1, 1]]);
+	// println!("{:?}", matrix - matrix_2);
+
+	// let matrix = Matrix(vec![vec![1, 1], vec![1, 1]]);
+	// let matrix_2 = Matrix(vec![vec![1, 1, 3], vec![1, 1]]);
+	// println!("{:?}", matrix - matrix_2);
+
+	// let matrix = Matrix(vec![vec![1, 3], vec![9, 1]]);
+	// let matrix_2 = Matrix(vec![vec![1, 1, 3], vec![1, 1]]);
+	// println!("{:?}", matrix + matrix_2);
+}
