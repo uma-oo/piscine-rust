4f9b2162ca73f8bb09054f038c78b6588fb9afd7
uma-oo <oumayma.elfahsi99@gmail.com>
2025-09-26 11:34:26 +0100

    add some code

---
+++ delete_prefix/src/lib.rs
@@ -0,0 +1,6 @@
+
+
+
+pub fn delete_prefix<'a> (prefix: &'a str, s: &'a str) -> Option<&'a str> {
+    if s.starts_with(prefix) {Some(&s[prefix.len()..])} else {None}
+}
---
+++ delete_prefix/src/main.rs
@@ -0,0 +1,8 @@
+
+
+use delete_prefix::delete_prefix;
+
+fn main() {
+	println!("{:?}", delete_prefix("ab", "abcdefghijklmnop"));
+	println!("{:?}", delete_prefix("x", "abcdefghijklmnop"));
+}
