3dc2dd4b4222f3867e35b8bf588b0ddf25d410cf
uma-oo <oumayma.elfahsi99@gmail.com>
2025-09-29 16:50:25 +0100

    add some code

--- iterators/src/lib.rs
+++ iterators/src/lib.rs
@@ -1,17 +1,21 @@
-#[derive(Copy, Clone, PartialEq, Debug)]
+#[derive(Copy, Clone, PartialEq, Debug, PartialOrd)]
 pub struct Collatz {
     pub v: u64,
 }
 
 impl Iterator for Collatz {
-    type Item = u64;
+    type Item = Self;
     fn next(&mut self) -> Option<Self::Item> {
+        let copy = *self;
+        if self.v == 1 {
+            return None; 
+        }
         if self.v % 2 == 0 {
             self.v = self.v / 2;
         } else {
             self.v = 3 * self.v + 1;
         }
-        Some(self.v)
+        Some(copy)
     }
 }
 
@@ -22,9 +26,9 @@ impl Collatz {
 }
 
 pub fn collatz(n: u64) -> usize {
-    let mut num = Collatz::new(n); 
+    let mut num = Collatz::new(n);
     let mut steps = 0;
-    while num!=(Collatz{v:1}) && num !=  (Collatz{v:0}){
+    while num != (Collatz { v: 1 }) && num !=( Collatz { v: 0 }) {
         num.next();
         steps += 1;
     }
